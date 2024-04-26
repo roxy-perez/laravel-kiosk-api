1e837597bfeb494f01039d84ec90a2aa4c120ba8
Roxy Pérez <necropsia32@gmail.com>
2024-04-26 17:32:29 +0100

    Added Order model and resources

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
@@ -22,6 +23,9 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    //Orders
+    Route::apiResource('/orders', OrderController::class);
 });
 
 Route::apiResource('categories', CategoryController::class);
